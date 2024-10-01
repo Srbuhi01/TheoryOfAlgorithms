ea0eaca1cd4cfcd2753c5b14d3588457b747e919
Srbuhi Pupuyan <srbuhi.004@gmail.com>
2024-10-01 21:13:26 +0400

    Add files via upload

---
+++ SelectionSort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+                
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+arr = [64, 25, 12, 22, 11, 100, -87]
+print("Original:", arr)
+selection_sort(arr)
+print("Sorted:", arr)
